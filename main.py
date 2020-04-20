b04ec5e79f55af0080af43ecd764b96f86fc6b59
bastihz <41594266+BastiHz@users.noreply.github.com>
2020-04-20 21:11:30 +0200

    Move the map with WASD

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ pygame.init()
 display = pygame.display.set_mode((640, 480))
 clock = pygame.time.Clock()
 
+BACKGROUND_COLOR = (0, 0, 0)
 COLORKEY = (255, 255, 255)
 tiles = []
 for filename in ("green.png", "red.png"):
@@ -44,18 +45,29 @@ TILE_HEIGHT_HALF = 16
 OFFSET_X = display.get_rect().width // 2 - TILE_WIDTH_HALF * len(MAP_DATA[0])
 OFFSET_Y = display.get_rect().height // 2 - TILE_HEIGHT_HALF
 
-for map_y, row in enumerate(MAP_DATA):
-    for map_x, i in enumerate(row):
-        tile = tiles[i]
-        display_x = (map_x + map_y) * TILE_WIDTH_HALF + OFFSET_X
-        display_y = (map_y - map_x) * TILE_HEIGHT_HALF + OFFSET_Y
-        display.blit(tile, (display_x, display_y))
-
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+            elif event.key == pygame.K_w:
+                OFFSET_Y -= TILE_HEIGHT_HALF
+            elif event.key == pygame.K_a:
+                OFFSET_X -= TILE_WIDTH_HALF
+            elif event.key == pygame.K_s:
+                OFFSET_Y += TILE_HEIGHT_HALF
+            elif event.key == pygame.K_d:
+                OFFSET_X += TILE_WIDTH_HALF
+
+    display.fill(BACKGROUND_COLOR)
+    for map_y, row in enumerate(MAP_DATA):
+        for map_x, i in enumerate(row):
+            tile = tiles[i]
+            display_x = (map_x + map_y) * TILE_WIDTH_HALF + OFFSET_X
+            display_y = (map_y - map_x) * TILE_HEIGHT_HALF + OFFSET_Y
+            display.blit(tile, (display_x, display_y))
+
     pygame.display.flip()
