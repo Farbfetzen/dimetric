d66f0c332d1018613e243d21b2cbfd47aaa72d1a
bastihz <41594266+BastiHz@users.noreply.github.com>
2020-09-18 19:30:37 +0200

    Restructure world.__init__() to make it more readable

--- src/world.py
+++ src/world.py
@@ -1,4 +1,4 @@
-"""Tile and world class."""
+"""World class managing all world objects."""
 
 # Copyright (C) 2020  Sebastian Henz
 #
@@ -26,71 +26,80 @@ from src.world_object import WorldObject
 
 class World:
     def __init__(self, world_data, images):
-        # FIXME: This init is very long. Maybe break it up into smaller functions?
         self.name = world_data["name"]
         self.sidelength = len(world_data["map"])
 
         # Check if map is square:
         for row in world_data["map"]:
-            if len(row) != len(world_data["map"]):
-                raise ValueError(f"Map '{self.name}' is not square.")
+            assert len(row) == self.sidelength, f"Map '{self.name}' is not square."
 
-        # Add a margin of some tile sizes so other stuff fits on the world surface, too.
-        self.margin_x = constants.TILE_WIDTH * 2
-        self.margin_bottom = constants.TILE_HEIGHT * 3
-        self.margin_top = constants.TILE_HEIGHT * 6
-        surf_width = self.sidelength * constants.TILE_WIDTH + self.margin_x * 2
-        surf_height = (self.sidelength * constants.TILE_HEIGHT
-                       + self.margin_top + self.margin_bottom)
-        self.surface = pygame.Surface((surf_width, surf_height))
+        # Overly long init split into multiple methods.
+        self.margin = pygame.Vector2()
+        self.offset = pygame.Vector2()
+        self.surface = None
+        self.rect = None
+        self.surf_pos = pygame.Vector2()
+        self.map_scroll_limit = pygame.Rect(0, 0, 0, 0)
+        self.init_surface()
+        self.visible_objects = []
+        self.map_tiles = []
+        self.init_tiles(world_data, images)
+        self.path = []
+        self.init_path(world_data)
 
-        # Position the map on the world surface. Offset is the position of
-        # (0, 0) world coordinates on the world surface.
-        self.offset_x = surf_width // 2
-        self.offset_y = self.margin_top
-
-        self.rect = self.surface.get_rect()
-        self.rect.center = (
-            constants.SMALL_DISPLAY_WIDTH // 2, 
-            constants.SMALL_DISPLAY_HEIGHT // 2
+        # highlight:
+        image = images["highlight"]
+        surface_pos = pygame.Vector2(self.world_pos_to_world_surf(0, 0))
+        surface_pos.x -= image.get_width() / 2
+        surface_pos.y -= (image.get_height() - constants.TILE_HEIGHT)
+        self.highlight = WorldObject(
+            "highlight",
+            image,
+            pygame.Vector2(0, 0),
+            surface_pos,
+            layer=-1
         )
-        # Use surf_pos to track the floating point position because
-        # rects can only hold integers.
-        self.surf_pos = pygame.Vector2(self.rect.topleft)
-
-        # Collision rect for limiting the map scrolling. Makes sure that
-        # parts of the map remain visible.
-        outer_margin_x = surf_width
-        outer_margin_y = surf_height
-        self.map_scroll_limit = pygame.Rect(
-            0,
-            0,
-            constants.SMALL_DISPLAY_WIDTH + outer_margin_x,
-            constants.SMALL_DISPLAY_HEIGHT + outer_margin_y
-        )
-        self.map_scroll_limit.center = self.rect.center
+        self.visible_objects.append(self.highlight)
 
         # Scroll can be -1, 0 or 1. Meaning left/up, none or right/down.
         self.scroll_direction = pygame.Vector2()
 
-        self.visible_objects = []  # Used for blitting
-        self.map_tiles = []  # Useful for finding a tile by world coordinates
+    def init_surface(self):
+        # Add a margin of some tile sizes so other stuff fits on the world surface, too.
+        self.margin.x = constants.TILE_WIDTH
+        self.margin.y = constants.TILE_HEIGHT * 3
+        surf_width = self.sidelength * constants.TILE_WIDTH + self.margin.x * 2
+        surf_height = self.sidelength * constants.TILE_HEIGHT + self.margin.y * 2
+        self.surface = pygame.Surface((surf_width, surf_height))
 
-        path_start = None
-        path_end = None
-        self.path_raw = []
+        # Position the map on the world surface. Offset is the position of
+        # (0, 0) world coordinates on the world surface.
+        self.offset.x = surf_width // 2
+        self.offset.y = self.margin.y
+
+        self.rect = self.surface.get_rect()
+        self.rect.center = (
+            constants.SMALL_DISPLAY_WIDTH // 2,
+            constants.SMALL_DISPLAY_HEIGHT // 2
+        )
+        # Use surf_pos to track the floating point position because
+        # rects can only hold integers.
+        self.surf_pos.update(self.rect.topleft)
+
+        # Collision rect for limiting the map scrolling. Makes sure that
+        # parts of the map remain visible.
+        self.map_scroll_limit.size = (
+            constants.SMALL_DISPLAY_WIDTH + surf_width,
+            constants.SMALL_DISPLAY_HEIGHT + surf_height
+        )
+        self.map_scroll_limit.center = self.rect.center
+
+    def init_tiles(self, world_data, images):
         for world_y, row in enumerate(world_data["map"]):
             self.map_tiles.append([])
             for world_x, symbol in enumerate(row):
                 tile_type = world_data["palette"][symbol]
                 world_pos = pygame.Vector2(world_x, world_y)
-                if tile_type == "path":
-                    self.path_raw.append(world_pos)
-                    if symbol == "S":
-                        path_start = world_pos
-                        self.path_raw.remove(world_pos)
-                    elif symbol == "E":
-                        path_end = world_pos
                 image = images[tile_type]
                 x, y = self.world_pos_to_world_surf(world_x, world_y)
                 # x and y locate the top corner of the base of the tile.
@@ -103,43 +112,43 @@ class World:
                 self.map_tiles[world_y].append(tile)
                 self.visible_objects.append(tile)
 
+    def init_path(self, world_data):
+        path_start = None
+        path_end = None
+        path_raw = []
+        for world_y, row in enumerate(world_data["map"]):
+            for world_x, symbol in enumerate(row):
+                tile_type = world_data["palette"][symbol]
+                world_pos = pygame.Vector2(world_x, world_y)
+                if tile_type == "path":
+                    path_raw.append(world_pos)
+                    if symbol == "S":
+                        path_start = world_pos
+                        path_raw.remove(world_pos)
+                    elif symbol == "E":
+                        path_end = world_pos
         if path_start is None or path_end is None:
             raise ValueError(f"Missing path start or end in map '{self.name}'.")
-        self.path = [path_start]
-        while self.path_raw:
+
+        self.path.append(path_start)
+        while path_raw:
             x, y = self.path[-1]
-            for neighbor in ((x+1, y), (x-1, y), (x, y+1), (x, y-1)):
-                if neighbor in self.path_raw:
+            for neighbor in ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)):
+                if neighbor in path_raw:
                     neighbor_ = pygame.Vector2(neighbor)
                     self.path.append(neighbor_)
-                    self.path_raw.remove(neighbor_)
+                    path_raw.remove(neighbor_)
                     break
             else:
                 raise ValueError(f"Malformed path in map '{self.name}': Neighbor not found.")
         if self.path[-1] != path_end:
             raise ValueError(f"Malformed path in map '{self.name}': Ends at wrong position.")
 
-        # highlight:
-        image = images["highlight"]
-        x, y = self.world_pos_to_world_surf(0, 0)
-        surface_pos = pygame.Vector2(
-            x - image.get_width() / 2,
-            y - (image.get_height() - constants.TILE_HEIGHT)
-        )
-        self.highlight = WorldObject(
-            "highlight",
-            image,
-            pygame.Vector2(0, 0),
-            surface_pos,
-            layer=-1
-        )
-        self.visible_objects.append(self.highlight)
-
     def world_pos_to_world_surf(self, world_x, world_y):
         # ATTENTION: Remember to account for the width and height of a sprite
         #   before blitting.
-        x = (world_x - world_y) * constants.TILE_WIDTH_HALF + self.offset_x
-        y = (world_x + world_y) * constants.TILE_HEIGHT_HALF + self.offset_y
+        x = (world_x - world_y) * constants.TILE_WIDTH_HALF + self.offset.x
+        y = (world_x + world_y) * constants.TILE_HEIGHT_HALF + self.offset.y
         return x, y
 
     def small_display_to_world_pos(self, x, y):
@@ -148,8 +157,8 @@ class World:
 
         # Get x and y relative to the topleft corner of the bounding rectangle
         # of the map (not the rect of the map surface):
-        x = x - self.rect.x - self.margin_x
-        y = y - self.rect.y - self.margin_top
+        x = x - self.rect.x - self.margin.x
+        y = y - self.rect.y - self.margin.y
 
         virt_x = x / constants.TILE_WIDTH
         virt_y = y / constants.TILE_HEIGHT
