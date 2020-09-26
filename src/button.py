4ee64571a996633b70d25982e128e9a2c11f7641
bastihz <41594266+BastiHz@users.noreply.github.com>
2020-09-26 17:12:44 +0200

    Make button text updatable and reduce amount of magic numbers

--- src/button.py
+++ src/button.py
@@ -16,32 +16,46 @@
 
 import pygame
 
+from src import constants
+
 
 class Button:
-    def __init__(self, text, size, position, action, align="centered"):
-        # FIXME: Lots of magic numbers and constants in this init.
+    def __init__(self, text, size, position, action):
         self.action = action
         self.rect = pygame.Rect((0, 0), size)
+        self.rect.center = position
         self.idle_image = pygame.Surface(size)
-        self.idle_image.fill((0, 0, 0))
+        self.idle_image.fill(constants.BUTTON_BACKGROUND_COLOR)
         self.hover_image = self.idle_image.copy()
-        self.hover_image.fill((64, 64, 64))
-        pygame.draw.rect(self.idle_image, (255, 255, 255), self.rect, 1)
-        pygame.draw.rect(self.hover_image, (255, 255, 255), self.rect, 1)
+        self.hover_image.fill(constants.BUTTON_BACKGROUND_COLOR_HOVER)
+        self.image_rect = self.idle_image.get_rect()
+        pygame.draw.rect(self.idle_image, constants.BUTTON_OUTLINE_COLOR,
+                         self.image_rect, 1)
+        pygame.draw.rect(self.hover_image, constants.BUTTON_OUTLINE_COLOR,
+                         self.image_rect, 1)
         self.image = self.idle_image
+        self.font = pygame.freetype.SysFont("sans", 15, bold=True)
+        self.font.fgcolor = constants.BUTTON_FONT_COLOR
+        self.text_rect = pygame.Rect(0, 0, 0, 0)
+        self.update_text(text)
 
-        font = pygame.freetype.SysFont("sans", 15, bold=True)
-        text_surf, text_rect = font.render(text, (255, 255, 255))
-        text_rect.center = self.rect.center
-        self.idle_image.blit(text_surf, text_rect)
-        self.hover_image.blit(text_surf, text_rect)
+    def update_text(self, text):
+        # First erase the previous text:
+        pygame.draw.rect(
+            self.idle_image,
+            constants.BUTTON_BACKGROUND_COLOR,
+            self.text_rect
+        )
+        pygame.draw.rect(
+            self.hover_image,
+            constants.BUTTON_BACKGROUND_COLOR_HOVER,
+            self.text_rect
+        )
 
-        if align == "centered":
-            self.rect.center = position
-        elif align == "topleft":
-            self.rect.topleft = position
-        else:
-            raise ValueError("Align must be either 'centered' or 'topleft'.")
+        text_surf, self.text_rect = self.font.render(text)
+        self.text_rect.center = self.image_rect.center
+        self.idle_image.blit(text_surf, self.text_rect)
+        self.hover_image.blit(text_surf, self.text_rect)
 
     def update(self, mouse_pos):
         if self.rect.collidepoint(mouse_pos.x, mouse_pos.y):
