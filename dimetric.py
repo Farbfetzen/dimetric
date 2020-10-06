edc919fda3dea91f9b3577950deaa31bbfb2ced0
Sebastian Henz <bastihz.dev@posteo.de>
2020-10-06 23:58:03 +0200

    Use default datetime format for logging

--- dimetric.py
+++ dimetric.py
@@ -22,7 +22,6 @@ logging.basicConfig(
     filename=".last_run.log",
     filemode="w",
     format="%(asctime)s %(levelname)s: %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
     level=logging.DEBUG
 )
 
